9d11d959f86893152225c1c20e836f9524cad821
Maciej Mionskowski <maciej.mionskowski@dynatrace.com>
2016-07-08 12:12:50 +0200

    Register test runs

--- src/com/dynatrace/integration/idea/execution/configuration/DynatraceRunConfigurationExtension.java
+++ src/com/dynatrace/integration/idea/execution/configuration/DynatraceRunConfigurationExtension.java
@@ -1,5 +1,6 @@
 package com.dynatrace.integration.idea.execution.configuration;
 
+import com.dynatrace.diagnostics.automation.rest.sdk.RESTEndpoint;
 import com.dynatrace.integration.idea.execution.DynatraceRunnerSettings;
 import com.dynatrace.integration.idea.plugin.DynatraceSettingsProvider;
 import com.intellij.execution.ExecutionException;
@@ -7,6 +8,9 @@ import com.intellij.execution.RunConfigurationExtension;
 import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.configurations.RunConfigurationBase;
 import com.intellij.execution.configurations.RunnerSettings;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.openapi.util.WriteExternalException;
@@ -14,7 +18,13 @@ import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DynatraceRunConfigurationExtension extends RunConfigurationExtension {
+    public static final Logger LOG = Logger.getLogger("DynatraceExtension");
 
     @Override
     public void updateJavaParameters(RunConfigurationBase configuration, JavaParameters javaParameters, RunnerSettings runnerSettings) throws ExecutionException {
@@ -24,14 +34,32 @@ public class DynatraceRunConfigurationExtension extends RunConfigurationExtensio
         }
 
         DynatraceSettingsProvider.State settings = DynatraceSettingsProvider.getInstance().getState();
-        DynatraceConfigurableStorage executionSettings = DynatraceConfigurableStorage.getOrCreateStorage(configuration);
-        StringBuilder builder = new StringBuilder("-agentpath:");
-        builder.append(settings.agent.agentLibrary).append('=');
-        builder.append("wait=").append(settings.server.timeout).append(',');
-        builder.append("name=").append(executionSettings.getAgentName()).append(',');
-        builder.append("server=").append(settings.agent.collectorHost).append(',');
-        builder.append("port=").append(settings.agent.collectorPort);//.append(',');
-        javaParameters.getVMParametersList().add(builder.toString());
+
+        try {
+            RESTEndpoint endpoint = new RESTEndpoint(settings.server.getLogin(), String.valueOf(settings.server.getPassword()), "https://" + settings.server.getHost() + ":" + settings.server.getPort());
+            DynatraceConfigurableStorage executionSettings = DynatraceConfigurableStorage.getOrCreateStorage(configuration);
+
+            StringBuilder builder = new StringBuilder("-agentpath:");
+            builder.append(settings.agent.agentLibrary).append('=');
+            builder.append("wait=").append(settings.server.timeout).append(',');
+            builder.append("name=").append(executionSettings.getAgentName()).append(',');
+            builder.append("server=").append(settings.agent.collectorHost).append(',');
+            builder.append("port=").append(settings.agent.collectorPort);
+
+            Calendar now = Calendar.getInstance();
+
+            //TODO: Don't run this if the configuration is not JUnit or NGTest
+            String id = endpoint.startTest(executionSettings.getSystemProfile(), String.valueOf(now.get(Calendar.YEAR)), String.valueOf(now.get(Calendar.MONTH) + 1), String.valueOf(now.get(Calendar.DAY_OF_WEEK)), String.valueOf(new SimpleDateFormat("HH:mm:ss").format(now.getTime())), null, null, null, null, null, null);
+
+            LOG.info("================" + id + "===================");
+            builder.append(',').append("optionTestRunIdJava=").append(id);
+
+            javaParameters.getVMParametersList().add(builder.toString());
+        } catch (Exception e) {
+            Notifications.Bus.notify(new Notification(Notifications.SYSTEM_MESSAGES_GROUP_ID, "Error occured", "<b>Check your configuration</b><br>"+e.getMessage(), NotificationType.ERROR));
+            LOG.log(Level.SEVERE,e.getMessage());
+            throw new ExecutionException(e);
+        }
     }
 
     @Override
