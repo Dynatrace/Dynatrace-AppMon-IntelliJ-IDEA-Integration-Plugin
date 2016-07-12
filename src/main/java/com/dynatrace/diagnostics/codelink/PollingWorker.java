13f2045df2df801f9d0c2c281db8c1f8bf714726
Maciej Mionskowski <maciej.mionskowski@dynatrace.com>
2016-07-12 12:37:29 +0200

    Moved Client creation and helper methods to utils class

--- src/main/java/com/dynatrace/diagnostics/codelink/PollingWorker.java
+++ src/main/java/com/dynatrace/diagnostics/codelink/PollingWorker.java
@@ -4,20 +4,12 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.ssl.SSLContextBuilder;
 import org.jetbrains.annotations.Nullable;
 
-import javax.net.ssl.SSLContext;
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,39 +25,6 @@ class PollingWorker implements Runnable {
                 .append("/rest/management/codelink/");
     }
 
-    /**
-     * Builds a custom {@link org.apache.http.impl.client.CloseableHttpClient} for Dynatrace client connection.
-     * TODO @see for dynatrace docs
-     *
-     * @return Configured {@link org.apache.http.impl.client.CloseableHttpClient}
-     * @see <a href="http://stackoverflow.com/questions/9402653/apache-httpclient-4-1-and-newer-how-to-do-basic-authentication" />
-     */
-    private static CloseableHttpClient createClient() {
-        try {
-            HttpClientBuilder builder = HttpClients.custom();
-            // trust all certs
-            builder.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE);
-            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, (chain, authType) -> true).build();
-            builder.setSslcontext(sslContext);
-
-            return builder.build();
-        } catch (Exception e) {
-            //impossible path
-            throw new RuntimeException("Error occured while creating custom CloseableHTTPClient in AppMonServerConfiguration", e);
-        }
-    }
-
-    private static <T> T inputStreamToObject(InputStream xml, Class<T> clazz) throws JAXBException, IOException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        try {
-            T obj = (T) unmarshaller.unmarshal(xml);
-            return obj;
-        } finally {
-            xml.close();
-        }
-    }
-
     private final CloseableHttpClient client;
     private final ICodeLinkSettings clSettings;
     private final IIDEDescriptor ide;
@@ -77,7 +36,7 @@ class PollingWorker implements Runnable {
     public PollingWorker(IIDEDescriptor ide, ICodeLinkSettings clSettings) {
         this.ide = ide;
         this.clSettings = clSettings;
-        this.client = PollingWorker.createClient();
+        this.client = Utils.createClient();
     }
 
     @Override
@@ -144,7 +103,7 @@ class PollingWorker implements Runnable {
 
         post.setEntity(new UrlEncodedFormEntity(nvps));
         try (CloseableHttpResponse response = this.client.execute(post)) {
-            CodeLinkLookupResponse lookup = inputStreamToObject(response.getEntity().getContent(), CodeLinkLookupResponse.class);
+            CodeLinkLookupResponse lookup = Utils.inputStreamToObject(response.getEntity().getContent(), CodeLinkLookupResponse.class);
             return lookup;
         }
     }
---
+++ src/main/java/com/dynatrace/diagnostics/codelink/Utils.java
@@ -0,0 +1,49 @@
+package com.dynatrace.diagnostics.codelink;
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+
+import javax.net.ssl.SSLContext;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Utils {
+    /**
+     * Builds a custom {@link org.apache.http.impl.client.CloseableHttpClient} for Dynatrace client connection.
+     * TODO @see for dynatrace docs
+     *
+     * @return Configured {@link org.apache.http.impl.client.CloseableHttpClient}
+     * @see <a href="http://stackoverflow.com/questions/9402653/apache-httpclient-4-1-and-newer-how-to-do-basic-authentication" />
+     */
+    public static CloseableHttpClient createClient() {
+        try {
+            HttpClientBuilder builder = HttpClients.custom();
+            // trust all certs
+            builder.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE);
+            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, (chain, authType) -> true).build();
+            builder.setSslcontext(sslContext);
+
+            return builder.build();
+        } catch (Exception e) {
+            //impossible path
+            throw new RuntimeException("Error occured while creating custom CloseableHTTPClient in AppMonServerConfiguration", e);
+        }
+    }
+
+    public static <T> T inputStreamToObject(InputStream xml, Class<T> clazz) throws JAXBException, IOException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        try {
+            T obj = (T) unmarshaller.unmarshal(xml);
+            return obj;
+        } finally {
+            xml.close();
+        }
+    }
+}
